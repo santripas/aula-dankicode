04ea114cbc419f110757ed23e0458ef060233e4b
Santripas <81268287+santripas@users.noreply.github.com>
2023-11-03 19:35:55 -0300

    estudos

--- aulas dio/aula6.js
+++ aulas dio/aula6.js
@@ -1,3 +1,8 @@
+
+
+/* ===========================  ARRAY ===============================
+
+
 class Pessoa {
     nome;
     idade;
@@ -6,4 +11,56 @@ class Pessoa {
         this.nome = nome;
         this.idade = idade;
     }
-}''
+}
+
+======================================================================*/
+
+const notas = [];
+console.log('Passou por aqui!');
+
+notas.push(5);
+notas.push(10);
+notas.push(10);
+
+
+let soma = 0;
+
+for (let i = 0; i < notas.length; i++) {
+    const nota = notas[i];
+    soma = soma + nota;
+} 
+    const media = soma / notas.length;
+    console.log(media);
+
+
+
+
+
+
+
+
+
+/*================================  FOR ==============================================
+
+const nome = 'Sandro';
+
+for (let i = 0; i < nome.length; i++) {
+    const letra = nome[i];
+    console.log(letra);    
+}
+
+==============================================================================================
+
+
+
+
+
+
+
+
+
+==============================================================================================
+{} - Bloco de código ou objeto literal
+[] - Lista
+==============================================================================================*/
+
---
+++ aulas dio/exerc4.js
@@ -0,0 +1,7 @@
+// 1 - Crie um programa que dado um número imprima a sua tabuada.
+
+
+
+for (let i = 0; i < 10; i++) {
+    console.log(i * 5);
+}
